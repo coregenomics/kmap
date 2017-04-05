98fb15fb0af7b81115f4e354fe9953f3e42967d9
Deivydas Giedrimas <giedrimd@gmail.com>
2017-04-05 16:51:06 -0400

    Delete commented code.

--- R/BiostringsProjectNEW.R
+++ R/BiostringsProjectNEW.R
@@ -17,32 +17,11 @@ library(tidyverse)
 # biocLite("BSgenome.Scerevisiae.UCSC.sacCer2")
 
 # # Subset to chromosome x for quick computation
-# genes <- c(BSgenome.Hsapiens.UCSC.hg19[[19]],BSgenome.Hsapiens.UCSC.hg19[[20]])
+
 
 # Create blacklist of unmappable regions, mappable regions w/ only ACGT
-# unmappable <- genes %>% mask("N") %>% gaps %>% as("IRanges")
-# mappable <- genes %>% mask("N") %>% as("IRanges")
-#
+
 # # Determine starts for reads of width 36, create views
-# starts_map <- mapply(seq,start(mappable),end(mappable)-36)
-# views_map <- IRanges::Views(genes, start=unlist(starts_map), width=36)
-# queries <- DNAStringSet(views_map)
-# system.time({PDict1 <- PDict(head(queries,100000))
-# alignment <- matchPDict(PDict1, chr19, max.mismatch = 0, min.mismatch = 0, with.indels = FALSE, fixed = TRUE, algorithm = "auto", verbose = FALSE)
-# hits <- sapply(alignment,length)>1
-# hit_sequences <- as(alignment, "CompressedIRangesList")%>%.[hits]%>%unlist%>%IRanges::reduce()
-# })
-# hit_sequences
-#
-# matches <- function(query){PDict1 <- PDict(query)
-# alignment <- matchPDict(PDict1, genes, max.mismatch = 0, min.mismatch = 0, with.indels = FALSE, fixed = TRUE, algorithm = "auto", verbose = FALSE)
-# hits <- sapply(alignment,length)>1
-# hit_sequences <- as(alignment, "CompressedIRangesList")%>%.[hits]%>%unlist%>%IRanges::reduce()
-# }
-# queries_list <- split(queries, ceiling(seq_along(queries)/100000))
-# MulticoreParam(progressbar = TRUE)
-# hits <- bplapply(head(queries_list), matches)
-# unmappable_ranges <- hits%>%c(unmappable)%>%as("List")%>%unlist()%>%IRanges::reduce()
 
 stddna_chrom <- function(dna_str){dna_ir <- dna_str %>% mask("N") %>% as("IRanges")}
 stddna <- function(bsgenome){
