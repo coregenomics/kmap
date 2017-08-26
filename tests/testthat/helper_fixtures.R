0caeba8ba048208ebdcc445bdd29ab4062212945
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-08-26 15:31:17 -0400

    TST: Fix helper first-time loading using explicit function references

--- tests/testthat/helper_fixtures.R
+++ tests/testthat/helper_fixtures.R
@@ -1,5 +1,3 @@
-library(Biostrings)
-
 ## Biostring genome.  Generating a mock BSgenome relies on having many
 ## disk files, which is not a very practical fixture.  Therefore use
 ## the smallest existing genome with unknown bases.  This Ecoli has 3
@@ -12,33 +10,34 @@ bsgenome <- BSgenome::getBSgenome(genome)
 .width <- 20
 .region_std <- 1
 .get_start <- function(pattern, width = 3) {
-    mask(bsgenome[[.seqname]], pattern = pattern) %>% gaps() %>%
-        .[width(.) == width] %>% head(1) %>% start()
+    Biostrings::mask(bsgenome[[.seqname]], pattern = pattern) %>%
+        GenomicRanges::gaps() %>% .[GenomicRanges::width(.) == width] %>%
+        head(1) %>% GenomicRanges::start()
 }
 .region_start <- .get_start("N")
 .region_middle <- .get_start("D", 1) - .width / 2
 .region_end <- .get_start("N", 2) - .width + 2
-.gr <- GRanges(seqnames = .seqname,
-               ranges = IRanges(
-                   start = c(.region_std, .region_start,
-                             .region_middle, .region_end),
-                   width = .width),
-               seqinfo = seqinfo(bsgenome))
-views <- Views(bsgenome, .gr)
+.gr <- GenomicRanges::GRanges(seqnames = .seqname,
+                              ranges = IRanges::IRanges(
+                                  start = c(.region_std, .region_start,
+                                            .region_middle, .region_end),
+                                  width = .width),
+                              seqinfo = GenomeInfoDb::seqinfo(bsgenome))
+views <- BSgenome::Views(bsgenome, .gr)
 views
 
 ## GRanges corresponding to view with no non-standard bases.
-.dnas <- DNAStringSet(views) %>% as.character()
+.dnas <- Biostrings::DNAStringSet(views) %>% as.character()
 ## Slow, but reliable way of checking for standard bases.
 .dna_ir <- function(dna_str) {
     bases <- unlist(strsplit(dna_str, split = NULL))
-    bases %in% DNA_BASES %>% Rle() %>% IRanges()
+    bases %in% Biostrings::DNA_BASES %>% S4Vectors::Rle() %>% IRanges::IRanges()
 }
 gr <- lapply(.dnas, .dna_ir) %>%
-    IRangesList %>%
-    shift(start(.gr) - 1) %>%
-    `names<-`(seqnames(.gr)) %>%
-    GRanges(seqinfo = seqinfo(bsgenome))
+    IRanges::IRangesList() %>%
+    GenomicRanges::shift(start(.gr) - 1) %>%
+    `names<-`(GenomeInfoDb::seqnames(.gr)) %>%
+    GenomicRanges::GRanges(seqinfo = GenomeInfoDb::seqinfo(bsgenome))
 
 ## kmer size smaller than gr .width
 kmer <- 10
