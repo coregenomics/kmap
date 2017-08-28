96e1d42b994f6eaaf81fa085fa7a6ce75d15f150
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2017-08-27 21:02:43 -0400

    TST: Try to workaround Travis CI BiocParallel multicore limitation

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,9 @@
 ## Unset R_TESTS as workaround for QuasR starting another R process.
 ## https://stackoverflow.com/a/27994299 # nolint
 Sys.setenv("R_TESTS" = "")
+## Workaround for BiocParallel multicore failing with Travis CI, issue 7052:
+## https://github.com/travis-ci/travis-ci/issues/7052 # nolint
+BiocParallel:: register(BiocParallel::SerialParam())
 
 library(testthat)
 library(kmap)
